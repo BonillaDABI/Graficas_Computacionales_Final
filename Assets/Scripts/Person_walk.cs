7deec1febfc7f0457ce792b939ac216b2c92e486
Bonilla7784 <115091301+BonillaDABI@users.noreply.github.com>
2023-07-28 16:14:47 -0600

    Update Person_wal.cs

--- Assets/Scripts/Person_walk.cs
+++ Assets/Scripts/Person_walk.cs
@@ -1,4 +1,4 @@
-//Scrip Movement
+//Scrip Movimiento
 //Ivan Eloy Bonilla Gameros A00826077
 //Alan Lespron A01194016
 using System.Collections;
@@ -7,40 +7,40 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    public float speed = 3.0f; // The speed at which the character moves
-    private Animator animator; // Reference to the Animator component
+    public float speed = 3.0f; // La velocidad a la que se mueve el personaje
+    private Animator animator; // Referencia al componente Animator
 
     void Start()
     {
-        // Get the Animator component
+        // Obtiene el componente Animator
         animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        // Get the horizontal and vertical input (WASD or arrow keys)
-        float horizontal = Input.GetAxis("Horizontal"); // A/D or Left/Right Arrow
-        float vertical = Input.GetAxis("Vertical"); // W/S or Up/Down Arrow
+        // Obtiene el input horizontal y vertical (WASD o teclas de flecha)
+        float horizontal = Input.GetAxis("Horizontal"); // A/D o Flecha Izquierda/Derecha
+        float vertical = Input.GetAxis("Vertical"); // W/S o Flecha Arriba/Abajo
 
-        // Create a new vector to store the direction of movement
+        // Crea un nuevo vector para almacenar la dirección del movimiento
         Vector3 movement = new Vector3(horizontal, 0, vertical);
 
-        // If there's some input (i.e., if the player is pressing a key)
+        // Si hay algún input (es decir, si el jugador está presionando una tecla)
         if (movement != Vector3.zero)
         {
-            // Set the isWalking parameter to true
+            // Establece el parámetro isWalking a verdadero
             animator.SetBool("isWalking", true);
 
-            // Rotate the character in the direction of movement
+            // Rota el personaje en la dirección del movimiento
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
 
-            // Move the character
+            // Mueve el personaje
             transform.Translate(movement * speed * Time.deltaTime, Space.World);
         }
         else
         {
-            // If there's no input (i.e., if the player isn't pressing any keys)
-            // Set the isWalking parameter to false
+            // Si no hay input (es decir, si el jugador no está presionando ninguna tecla)
+            // Establece el parámetro isWalking a falso
             animator.SetBool("isWalking", false);
         }
     }
